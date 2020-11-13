aa2783df6c6f21b04051b05e02160768931fafdb
rubetube95 <rubenocallaghan@hotmail.com>
2020-11-13 17:38:58 +0000

    Update ConcatenatedResultsToCSV.py

--- ConcatenatedResultsToCSV.py
+++ ConcatenatedResultsToCSV.py
@@ -2,6 +2,23 @@
 # SAVE THIS FILE INTO FOLDER WHERE DATA RESIDES. THEN CHANGE *.data TO MATCH FILETYPE *.csv or *.txt AND RUN.
 # After running, if data in column has data seperated by spaces within the same column use Excel's Data>Text to Columns > Delimited to seperate into seperate columns
 #
+import glob
+import os
+
+out_filename = "combinedFiles.csv"
+if os.path.exists(out_filename):
+    os.remove(out_filename)
+
+read_files = glob.glob("*.data-nonbm")#may have to use exact filetype e.g. *.data
+with open(out_filename, "w") as outfile:
+    for filename in read_files:
+        with open(filename) as infile:
+            for line in infile:
+                outfile.write('{},{}\n'.format(line.strip(), filename))
+
+
+                
+                # one below is broken - deletes one entry from each file - need to fix - one above works
 import os
 import glob
 
@@ -23,3 +40,6 @@ with open(out_filename, "w") as outfile:
                 next(infile)
             for line in infile:
                 outfile.write('{},{}\n'.format(line.strip(), filename))
+
+                
+                
